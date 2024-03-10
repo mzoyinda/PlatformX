eb47d30394b55b66b74e9d959e7c43344fbb0a64
oyindamola <oyindamoladawodu22@gmail.com>
2024-03-10 08:12:23 +0100

    event template added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,14 +7,14 @@ import Speaker from "@/components/Speaker";
 
 export default function Home() {
   return (
-<main class="w-full md:flex flex-col justify-center items-center">
-<div className="w-full max-w-[1800px]">
-  <Header/>
-  <About/>
-  <Speaker/>
-{/* <EventSchedule/> */}
-  <Footer/>
-</div>
- </main>
+    <main class="w-full md:flex flex-col justify-center items-center">
+      <div className="w-full max-w-[1800px]">
+        <Header />
+        <About />
+        <Speaker />
+        <EventSchedule />
+        <Footer />
+      </div>
+    </main>
   );
 }
--- src/components/EventSchedule.jsx
+++ src/components/EventSchedule.jsx
@@ -6,17 +6,26 @@ const EventSchedule = () => {
       <div className="w-[150px] mb-4 h-[3px] bg-[#fa3f01]" />
       <h3 className="text-3xl mb[-5px] ">Event Schedule</h3>
       <div className="gallery mt-12 md:mt-24 flex flex-wrap justify-center md:flex-row md:items-center">
-        {/* <h2>Coming Soon...</h2> */}
+        <div className="container mx-auto px-4">
+      <div className="flex w-full justify-center items-center flex-col gap-8">
+    
 
-        <div className="container mx-auto px-4 py-8">
-      {/* <h2 className="text-3xl font-bold text-center mb-8">Event Schedule</h2> */}
-      <div className="flex flex-col gap-8">
-        <div className="program bg-white rounded-lg shadow-md p-6">
-          <h3 className="text-lg font-bold mb-2">Program 1</h3>
-          <p className="text-gray-600 mb-4">Time: 10:00 AM - 11:00 AM</p>
+        <div className="program px-4 sm:px-16 py-4 w-[80vw] max-w-[1000px] lg:w-[60vw] text-black flex col-reverse flex-col sm:flex-row sm:justify-between sm:items-center bg-white rounded-lg shadow-md">
+          <p className="text-gray-600 font-bold mb-2">10:00 AM - 11:00 AM</p>
+          <div>
+          <h3 className="text-lg mb-4">Program 1 </h3>
           <p className="text-gray-800">Description of Program 1.</p>
+          </div>
         </div>
-   
+
+        <div className="program px-4 sm:px-16 py-4 w-[80vw] max-w-[1000px] lg:w-[60vw] text-black flex col-reverse flex-col sm:flex-row sm:justify-between sm:items-center bg-white rounded-lg shadow-md">
+          <p className="text-gray-600 font-bold mb-2">10:00 AM - 11:00 AM</p>
+          <div>
+          <h3 className="text-lg mb-4">Program 1 </h3>
+          <p className="text-gray-800">Description of Program 1.</p>
+          </div>
+        </div>
+        
       </div>
     </div>
 
